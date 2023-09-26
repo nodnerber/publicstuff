1774e787d367f26d81483d82e18c339102ff8fd5
nodnerber <126204000+nodnerber@users.noreply.github.com>
2023-09-25 20:42:59 -0400

    Add files via upload

---
+++ mountmydisk.ps1
@@ -0,0 +1,14 @@
+$disk = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+    $letters = 70..89 | ForEach-Object { [char]$_ }
+    $count = 0
+    $labes = "data1","data2"
+
+    foreach ($disk in $disks) {
+        $driveLetter = $letters[$count].ToString()
+        $disk |
+        Initialize-Disk -PartitionStyle MBR -PassThru |
+        New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel, $labels[$count] -Confirm:$false -Force
+    $count++
+    }
