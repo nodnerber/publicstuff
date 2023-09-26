d876a2b239345574330837fff65c38e63e4fd6b2
nodnerber <126204000+nodnerber@users.noreply.github.com>
2023-09-25 21:03:06 -0400

    Update mountmydisk.ps1

--- mountmydisk.ps1
+++ mountmydisk.ps1
@@ -2,7 +2,7 @@ $disk = Get-Disk | Where partitionstyle -eq 'raw' | sort number
 
     $letters = 70..89 | ForEach-Object { [char]$_ }
     $count = 0
-    $labes = "data1","data2"
+    $labels = "data1","data2"
 
     foreach ($disk in $disks) {
         $driveLetter = $letters[$count].ToString()
